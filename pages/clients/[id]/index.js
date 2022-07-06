1c47adfb39412e83f271c33ebcdb2c2aae9e78f8
Manoj Rai <manojthulung03@gmail.com>
2022-07-06 23:40:40 +0545

    added Link to the pages

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
+
 function ClientProject() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    router.push("/clients/jhn/Project"); //redirect to the given path
+  }
+
   return (
     <div>
       <h1>The Project of a given client</h1>
+      <button onClick={loadProjectHandler}>Load a Project</button>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 function Home() {
   return (
     <div>
       <h1>Hello world</h1>
+      <ul>
+        <li>
+          <Link href="clients">Clients</Link>
+        </li>
+        <li>
+          <Link href="portfolio">Portfolio</Link>
+        </li>
+      </ul>
     </div>
   );
 }
